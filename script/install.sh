da70a5d8eb741665426aa08e94854af272cd6cad
cyan92128505 <cyan92128505@gmail.com>
2024-02-09 22:21:49 +0800

    🐳 chore: Add dependencies

--- script/install.sh
+++ script/install.sh
@@ -1,11 +1,12 @@
 fvm flutter pub add  \
+audio_video_progress_bar \
 file_picker_pro \
 flutter_hooks \
 flutter_svg \
 freezed_annotation \
+go_router_plus \
 hive \
 hive_flutter \
-go_router_plus \
 hooks_riverpod \
 just_audio \
 just_audio_background \
@@ -14,5 +15,6 @@ dev:build_runner \
 dev:custom_lint \
 dev:freezed \
 dev:hive_generator \
+dev:json_serializable \
 dev:riverpod_generator \
 dev:riverpod_lint
