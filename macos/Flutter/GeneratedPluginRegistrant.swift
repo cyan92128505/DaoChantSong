3bf5c0ee899f42c3dc5c2177ff4cc495590fe570
cyan92128505 <cyan92128505@gmail.com>
2024-02-09 15:47:40 +0800

    🐳 chore: Add dependencies

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,20 +5,24 @@
 import FlutterMacOS
 import Foundation
 
+import audio_service
 import audio_session
 import file_picker_pro
 import file_selector_macos
 import just_audio
 import open_share_plus
 import path_provider_foundation
+import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioServicePlugin.register(with: registry.registrar(forPlugin: "AudioServicePlugin"))
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   FilePickerProPlugin.register(with: registry.registrar(forPlugin: "FilePickerProPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   OpenSharePlusPlugin.register(with: registry.registrar(forPlugin: "OpenSharePlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
--- script/install.sh
+++ script/install.sh
@@ -2,11 +2,17 @@ fvm flutter pub add  \
 file_picker_pro \
 flutter_hooks \
 flutter_svg \
+freezed_annotation \
+hive \
+hive_flutter \
 go_router_plus \
 hooks_riverpod \
 just_audio \
+just_audio_background \
 riverpod_annotation \
 dev:build_runner \
 dev:custom_lint \
+dev:freezed \
+dev:hive_generator \
 dev:riverpod_generator \
 dev:riverpod_lint
